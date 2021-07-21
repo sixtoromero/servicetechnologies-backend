fe13153bc93e2910c8a78921861ba94bb7d585a9
sixtux <sixto.jose@gmail.com>
2021-07-21 17:11:16 -0500

    Primer commit

---
+++ app/Controllers/API/Users.php
@@ -0,0 +1,285 @@
+<?php namespace App\Controllers\API;
+
+use CodeIgniter\RESTful\ResourceController;
+use App\Models\UsersModel;
+
+class Users extends ResourceController
+{
+    public function __construct() {
+        $this->model = $this->setModel(new UsersModel());
+    }
+
+	public function index()
+	{
+		$users = $this->model->findAll();
+		$json = array(
+			"status" => 200,
+			"message" => '',
+			"data"=>$users
+		);
+
+		return $this->respond($json);
+	}
+
+	public function create(){
+		
+		$validation =  \Config\Services::validation();
+
+		try {
+
+			//Validar datos
+			$validation->setRules([
+				'names' => 'required|string|max_length[255]',
+				'surnames' => 'required|string|max_length[255]',
+				'usertype' => 'required|string|max_length[255]',
+				'email' => 'required|valid_email|is_unique[users.email]',
+				'usertype' => 'required|string|max_length[120]',
+				'address' => 'required|string|max_length[255]',
+				'cellphone' => 'required|string|max_length[20]',
+				'password' => 'required|string|max_length[255]'
+			]);
+	
+			$validation->withRequest($this->request)
+			   ->run();
+
+			if ($validation->getErrors()){
+				
+				$errors = $validation->getErrors();
+
+				$json = array(
+					"status" => 404,
+					"message" => 'Las validaciones no pasaron',
+					"data"=>$errors
+				);
+					
+				return $this->respond($json);
+			}
+
+			$users = $this->request->getJSON();
+			if($this->model->insert($users)):
+
+				$users->id = $this->model->insertID();				
+				
+				$json = array(
+					"status" => 201,
+					"message" => 'successfull',
+					"data"=>$users
+				);
+
+				return $this->respondCreated($json);
+			else:
+				$json = array(
+					"status" => 404,
+					"message" => 'The record could not be inserted please try again',
+					"data"=> null
+				);
+					
+				return $this->respond($json);
+				//return $this->failValidationError($this->model->validation->listErros());
+			endif;
+		}
+		catch (\Exception $e) {
+			//return $this->failServerError('An error has occurred on the server.');
+			$json = array(
+				"status" => 404,
+				"message" => 'An error has occurred on the server.',
+				"data"=> null
+			);
+				
+			return $this->respond($json);
+		}
+	}
+
+
+	public function FindById($id = null){
+		try {
+
+			if ($id == null) {
+				$json = array(
+					"status" => 404,
+					"message" => 'A valid Id has not been passed',
+					"data"=> null
+				);
+					
+				return $this->respond($json);
+			}
+
+			$users = $this->model->find($id);
+
+			if ($users == null) {
+				$json = array(
+					"status" => 404,
+					"message" => 'Record with Id not found '. $id,
+					"data"=> null
+				);					
+				return $this->respond($json);
+			}
+
+			$json = array(
+				"status" => 200,
+				"message" => 'Found record',
+				"data"=>$users
+			);
+	
+			return $this->respond($json);
+
+		}
+		catch (\Exception $e) {
+			$json = array(
+				"status" => 404,
+				"message" => $e,
+				"data"=>$users
+			);
+	
+			return $this->respond($json);
+		}
+	}
+
+	public function edit($id = null) {
+		try {
+			if ($id == null) {
+				$json = array(
+					"status" => 404,
+					"message" => 'A valid Id has not been passed',
+					"data"=> null
+				);
+					
+				return $this->respond($json);
+			}
+
+			$users = $this->model->find($id);
+
+			if ($users == null) {
+				$json = array(
+					"status" => 404,
+					"message" => 'Record with Id not found '. $id,
+					"data"=> null
+				);
+				return $this->respond($json);
+			}
+
+			$json = array(
+				"status" => 200,
+				"message" => '',
+				"data"=>$users
+			);
+	
+			return $this->respond($json);
+
+
+
+		} catch (\Throwable $th) {
+			return $this->failServerError('An error has occurred on the server.');
+		}
+	}
+
+	public function update($id = null){
+		try {
+
+			if ($id == null) {
+				$json = array(
+					"status" => 404,
+					"message" => 'A valid Id has not been passed',
+					"data"=> null
+				);
+					
+				return $this->respond($json);
+			}
+
+			$usersVerified = $this->model->find($id);
+
+			if ($usersVerified == null) {
+				$json = array(
+					"status" => 404,
+					"message" => 'Record with Id not found '. $id,
+					"data"=> null
+				);
+				return $this->respond($json);
+			}						
+
+			$users = $this->request->getJSON();			
+
+			if($this->model->update($id, $users)):				
+				$users->id = $id;
+				$json = array(
+					"status" => 200,
+					"message" => 'successfull',
+					"data"=>$users
+				);
+				return $this->respondUpdated($json);
+			else:
+				$json = array(
+					"status" => 404,
+					"message" => 'The record could not be updated please try again',
+					"data"=> null
+				);
+					
+				return $this->respond($json);
+				//return $this->failValidationError($this->model->validation->listErros());
+			endif;
+		}
+		catch (\Exception $e) {
+			$json = array(
+				"status" => 404,
+				"message" => $e,
+				"data"=>$users
+			);
+	
+			return $this->respond($json);
+		}
+	}
+
+	public function delete($id = null){
+		try {
+
+			if ($id == null) {
+				$json = array(
+					"status" => 404,
+					"message" => 'A valid Id has not been passed',
+					"data"=> null
+				);
+					
+				return $this->respond($json);
+			}
+
+			$usersVerified = $this->model->find($id);
+
+			if ($usersVerified == null) {
+				$json = array(
+					"status" => 404,
+					"message" => 'Record with Id not found '. $id,
+					"data"=> null
+				);
+				return $this->respond($json);
+			}						
+			
+			if	($this->model->delete($id)):
+				$json = array(
+					"status" => 200,
+					"message" => 'Registry removed successfully.',
+					"data"=>$users
+				);
+				return $this->respondDeleted($json);
+			else:
+				$json = array(
+					"status" => 404,
+					"message" => 'The record could not be deleted.',
+					"data"=> null
+				);
+				return $this->respond($json);
+			endif;
+
+		}
+		catch (\Exception $e) {
+			$json = array(
+				"status" => 404,
+				"message" => $e,
+				"data"=>$users
+			);
+	
+			return $this->respond($json);
+		}
+	}
+
+
+}
